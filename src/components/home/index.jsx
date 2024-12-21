bccdaa69ebebf9dde4c08bb99914a2d8fa31a251
Balaji Sankarapu <balajisankarapu@Balajis-MacBook-Pro.local>
2024-12-21 13:34:23 -0600

    pushing client code

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="home">
       <h1>Home</h1>
+      <Link to="/">
+						<button type="button" >
+							Sing Up
+						</button>
+					</Link>
     </div>
   );
 }
